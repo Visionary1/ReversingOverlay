ae10554a4b007e3789ae4290444e9081dd22e3b4
rl144 <koreacnc3@gmail.com>
2015-05-01 00:44:20 +0900

    Fiora.cs Updated
    심플하게

--- ALL In One/champions/Fiora.cs
+++ ALL In One/champions/Fiora.cs
@@ -1,26 +1,21 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 using LeagueSharp;
 using LeagueSharp.Common;
+using SharpDX;
 
 using Color = System.Drawing.Color;
 
-using System.Runtime.ExceptionServices;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using System.IO;
-using SharpDX;
 
 namespace ALL_In_One.champions
 {
     class Fiora // RL244
     {
-        static Menu Menu { get { return AIO_Menu.MainMenu_Manual; } }
+        static Menu Menu {get{return AIO_Menu.MainMenu_Manual.SubMenu("Champion");}} // 피오라.cs update.
         static Orbwalking.Orbwalker Orbwalker { get { return AIO_Menu.Orbwalker; } }
         static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
         static Spell Q, W, E, R;
@@ -36,34 +31,35 @@ namespace ALL_In_One.champions
             E = new Spell(SpellSlot.E);
             R = new Spell(SpellSlot.R, 400f, TargetSelector.DamageType.Physical);
 
-            Q.SetTargetted(0.25f, float.MaxValue);
+            Q.SetTargetted(0.25f, 3000f);
 
-            Menu.SubMenu("Champion").SubMenu("Combo").AddItem(new MenuItem("CbUseQ", "Use Q", true).SetValue(true));
+            AIO_Menu.Champion.Combo.addUseQ();
             Menu.SubMenu("Champion").SubMenu("Combo").AddItem(new MenuItem("CbUseQD", "Q Distance", true).SetValue(new Slider(150, 0, 600)));
-            Menu.SubMenu("Champion").SubMenu("Combo").AddItem(new MenuItem("CbUseW", "Use W", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Combo").AddItem(new MenuItem("CbUseE", "Use E", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Combo").AddItem(new MenuItem("CbUseR", "Use R", true).SetValue(true));
+            AIO_Menu.Champion.Combo.addUseW();
+            AIO_Menu.Champion.Combo.addUseE();
+            AIO_Menu.Champion.Combo.addUseR();
 
-            Menu.SubMenu("Champion").SubMenu("Harass").AddItem(new MenuItem("HrsUseE", "Use E", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Harass").AddItem(new MenuItem("HrsMana", "if Mana % >", true).SetValue(new Slider(60, 0, 100)));
-
-            Menu.SubMenu("Champion").SubMenu("Laneclear").AddItem(new MenuItem("LcUseE", "Use E", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Laneclear").AddItem(new MenuItem("LcMana", "if Mana % >", true).SetValue(new Slider(60, 0, 100)));
-
-            Menu.SubMenu("Champion").SubMenu("Jungleclear").AddItem(new MenuItem("JcUseQ", "Use Q", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Jungleclear").AddItem(new MenuItem("JcUseW", "Use W", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Jungleclear").AddItem(new MenuItem("JcUseE", "Use E", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Jungleclear").AddItem(new MenuItem("JcMana", "if Mana % >", true).SetValue(new Slider(20, 0, 100)));
-
-            Menu.SubMenu("Champion").SubMenu("Misc").AddItem(new MenuItem("credit", "Made By RL244", true));
-            Menu.SubMenu("Champion").SubMenu("Misc").AddItem(new MenuItem("miscKsQ", "Use Q KillSteal", true).SetValue(true));
-            Menu.SubMenu("Champion").SubMenu("Misc").AddItem(new MenuItem("miscKsR", "Use R KillSteal", true).SetValue(true));
+            AIO_Menu.Champion.Harass.addUseW();
+            AIO_Menu.Champion.Harass.addUseE();
+            AIO_Menu.Champion.Harass.addIfMana();
 			
-            Menu.SubMenu("Champion").SubMenu("Drawings").AddItem(new MenuItem("drawQ", "Q Range", true).SetValue(new Circle(true, Color.Red)));
-            Menu.SubMenu("Champion").SubMenu("Drawings").AddItem(new MenuItem("drawR", "R Range", true).SetValue(new Circle(true, Color.Blue)));
-            Menu.SubMenu("Champion").SubMenu("Drawings").AddItem(new MenuItem("drawQTimer", "Q Timer", true).SetValue(new Circle(true, Color.LightGreen)));
-            Menu.SubMenu("Champion").SubMenu("Drawings").AddItem(new MenuItem("drawWTimer", "W Timer", true).SetValue(new Circle(true, Color.Black)));
-            Menu.SubMenu("Champion").SubMenu("Drawings").AddItem(new MenuItem("drawETimer", "E Timer", true).SetValue(new Circle(true, Color.Red)));
+            AIO_Menu.Champion.Laneclear.addUseE();
+            AIO_Menu.Champion.Laneclear.addIfMana();
+			
+            AIO_Menu.Champion.Jungleclear.addUseQ();
+            AIO_Menu.Champion.Jungleclear.addUseW();
+			AIO_Menu.Champion.Jungleclear.addUseE();
+            AIO_Menu.Champion.Jungleclear.addIfMana();
+
+            AIO_Menu.Champion.Misc.addItem("Made By Rl244", true);
+            AIO_Menu.Champion.Misc.addItem("KillstealQ", true);
+            AIO_Menu.Champion.Misc.addItem("KillstealR", true);
+			
+            AIO_Menu.Champion.Drawings.addQRange();
+            AIO_Menu.Champion.Drawings.addRRange();
+            AIO_Menu.Champion.Drawings.addItem("Q Timer", new Circle(true, Color.Blue));
+            AIO_Menu.Champion.Drawings.addItem("W Timer", new Circle(true, Color.Black));
+            AIO_Menu.Champion.Drawings.addItem("E Timer", new Circle(true, Color.Red));
 
 			AIO_Menu.Champion.Drawings.addDamageIndicator(getComboDamage);
 
@@ -81,23 +77,17 @@ namespace ALL_In_One.champions
 
             if (Orbwalking.CanMove(10))
             {
-                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
+                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo) // 하레스 넣을 필요가 없어서 뺌. (AA에 하레스 있음)
                     Combo();
 
-                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
-                    Harass();
-
-                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear)
-                {
-                    Laneclear();
+                if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear) // 라인클리어 - 라인클리어도 필요없어서 뺌. (AAJcLc에 포함)
                     Jungleclear();
-                }
             }
 
             #region Killsteal
-            if (Menu.Item("miscKsQ", true).GetValue<bool>())
+            if (AIO_Menu.Champion.Misc.getBoolValue("KillstealQ"))
                 KillstealQ();
-            if (Menu.Item("miscKsR", true).GetValue<bool>())
+            if (AIO_Menu.Champion.Misc.getBoolValue("KillstealR"))
                 KillstealR();
             #endregion
 			
@@ -113,31 +103,24 @@ namespace ALL_In_One.champions
             if (Player.IsDead)
                 return;
 
-            var drawQ = Menu.Item("drawQ", true).GetValue<Circle>();
-            var drawR = Menu.Item("drawR", true).GetValue<Circle>();
-            var drawQTimer = Menu.Item("drawQTimer", true).GetValue<Circle>();
-			var drawWTimer = Menu.Item("drawWTimer", true).GetValue<Circle>();
-            var drawETimer = Menu.Item("drawETimer", true).GetValue<Circle>();
-
+            var drawQ = AIO_Menu.Champion.Drawings.QRange;
+            var drawR = AIO_Menu.Champion.Drawings.RRange;
+            var drawQTimer = AIO_Menu.Champion.Drawings.getCircleValue("Q Timer");
+			var drawWTimer = AIO_Menu.Champion.Drawings.getCircleValue("W Timer");
+            var drawETimer = AIO_Menu.Champion.Drawings.getCircleValue("E Timer");
+			var pos_temp = Drawing.WorldToScreen(Player.Position);
+			
             if (Q.IsReady() && drawQ.Active)
                 Render.Circle.DrawCircle(Player.Position, Q.Range, drawQ.Color);
             if (R.IsReady() && drawR.Active)
                 Render.Circle.DrawCircle(Player.Position, R.Range, drawR.Color);
+
             if (drawQTimer.Active && getQBuffDuration > 0)
-            {
-                var pos_temp = Drawing.WorldToScreen(Player.Position);
                 Drawing.DrawText(pos_temp[0], pos_temp[1], drawQTimer.Color, "Q: " + getQBuffDuration.ToString("0.00"));
-            }
             if (drawWTimer.Active && getWBuffDuration > 0)
-            {
-                var pos_temp = Drawing.WorldToScreen(Player.Position);
                 Drawing.DrawText(pos_temp[0], pos_temp[1], drawWTimer.Color, "W: " + getWBuffDuration.ToString("0.00"));
-            }
             if (drawETimer.Active && getEBuffDuration > 0)
-            {
-                var pos_temp = Drawing.WorldToScreen(Player.Position);
                 Drawing.DrawText(pos_temp[0], pos_temp[1], drawETimer.Color, "E: " + getEBuffDuration.ToString("0.00"));
-            }
         }
 		
 	static Obj_AI_Minion Chase(Vector3 pos)
@@ -175,8 +158,11 @@ namespace ALL_In_One.champions
         static void Obj_AI_Hero_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
             var Mobs = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
-			if (Mobs.Count >= 1 && (!Menu.Item("JcUseW", true).GetValue<bool>() || !(AIO_Func.getManaPercent(Player) > Menu.Item("JcMana", true).GetValue<Slider>().Value)) ||
-			Mobs.Count == 0 && !Menu.Item("CbUseW", true).GetValue<bool>())
+			if ((Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear && Mobs.Count >= 1 &&
+			(!AIO_Menu.Champion.Jungleclear.UseW || !(AIO_Func.getManaPercent(Player) > AIO_Menu.Champion.Jungleclear.IfMana))) ||
+			Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && !AIO_Menu.Champion.Combo.UseW || !AIO_Menu.Champion.Harass.UseW ||
+			Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed && !(AIO_Func.getManaPercent(Player) > AIO_Menu.Champion.Harass.IfMana))
+			return;
             if (IsOnHit(args.SData.Name) && args.Target.IsMe && Player.Distance(args.End) < 150)
 			W.Cast();
         }
@@ -185,7 +171,7 @@ namespace ALL_In_One.champions
 	{
 		if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed)
 		{	
-			if (Menu.Item("HrsUseE", true).GetValue<bool>() && E.IsReady()
+			if (AIO_Menu.Champion.Harass.UseE && E.IsReady()
 				&& HeroManager.Enemies.Any(x => Orbwalking.InAutoAttackRange(x))
 			&& utility.Activator.AfterAttack.ALLCancelItemsAreCasted)
 				E.Cast();
@@ -194,14 +180,14 @@ namespace ALL_In_One.champions
 			
 		if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo)
 		{
-			if (Menu.Item("CbUseE", true).GetValue<bool>() && E.IsReady()
+			if (AIO_Menu.Champion.Combo.UseE && E.IsReady()
 				&& HeroManager.Enemies.Any(x => Orbwalking.InAutoAttackRange(x))
 				&& utility.Activator.AfterAttack.ALLCancelItemsAreCasted)
 				E.Cast();
 				
 			foreach (var rtarget in HeroManager.Enemies.OrderByDescending(x => x.Health))
 			{
-			if (Menu.Item("CbUseR", true).GetValue<bool>() && R.IsReady()
+			if (AIO_Menu.Champion.Combo.UseR && R.IsReady()
 			&& utility.Activator.AfterAttack.ALLCancelItemsAreCasted && !E.IsReady()
 			&& HeroManager.Enemies.Any(x => x.IsValidTarget(R.Range))
 			&& AIO_Func.isKillable(rtarget,R.GetDamage(rtarget) + Q.GetDamage(rtarget)*2))
@@ -216,17 +202,7 @@ namespace ALL_In_One.champions
 			return;
 
 		if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.LaneClear)
-		{
-			var Minions = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Enemy);
-			var Mobs = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
-			if(Minions.Count + Mobs.Count <= 0)
-			return;
-			
-			if(Minions.Count >= 1)
-			AALaneclear();
-			if(Mobs.Count >= 1)
-			AAJungleclear();
-		}
+			AIO_Func.AALcJc(E);
 		
 		if(!utility.Activator.AfterAttack.AIO)
 			AA();
@@ -234,7 +210,7 @@ namespace ALL_In_One.champions
 
         static void Combo()
         {
-            if (Menu.Item("CbUseQ", true).GetValue<bool>() && Q.IsReady()) 
+            if (AIO_Menu.Champion.Combo.UseQ && Q.IsReady()) 
 			{
 				var qd = Menu.Item("CbUseQD", true).GetValue<Slider>().Value;
 				var qTarget = TargetSelector.GetTarget(Q.Range, Q.DamageType);
@@ -252,54 +228,9 @@ namespace ALL_In_One.champions
 			}
         }
 
-        static void Harass()
-        {
-            if (!(AIO_Func.getManaPercent(Player) > Menu.Item("HrsMana", true).GetValue<Slider>().Value))
-                return;
-
-        }
-
-        static void AALaneclear()
-        {
-            if (!(AIO_Func.getManaPercent(Player) > Menu.Item("LcMana", true).GetValue<Slider>().Value))
-                return;
-
-				var Minions = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Enemy);
-
-				if (Minions.Count <= 0)
-                return;
-				
-                if (Menu.Item("LcUseE", true).GetValue<bool>() && E.IsReady())
-				E.Cast();
-        }
-
-        static void AAJungleclear()
-        {
-            if (!(AIO_Func.getManaPercent(Player) > Menu.Item("JcMana", true).GetValue<Slider>().Value))
-                return;
-
-            var Mobs = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
-
-            if (Mobs.Count <= 0)
-                return;
-				
-                if (Menu.Item("JcUseE", true).GetValue<bool>() && E.IsReady())
-				E.Cast();
-        }
-		
-		
-        static void Laneclear()
-        {
-            if (!(AIO_Func.getManaPercent(Player) > Menu.Item("LcMana", true).GetValue<Slider>().Value))
-                return;
-
-            var Minions = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Enemy);
-
-        }
-
         static void Jungleclear()
         {
-            if (!(AIO_Func.getManaPercent(Player) > Menu.Item("JcMana", true).GetValue<Slider>().Value))
+            if (!(AIO_Func.getManaPercent(Player) > AIO_Menu.Champion.Jungleclear.IfMana))
                 return;
 
             var Mobs = MinionManager.GetMinions(Q.Range, MinionTypes.All, MinionTeam.Neutral, MinionOrderTypes.MaxHealth);
@@ -307,12 +238,7 @@ namespace ALL_In_One.champions
             if (Mobs.Count <= 0)
                 return;
 
-				
-            if (Menu.Item("JcUseW", true).GetValue<bool>() && W.IsReady())
-                W.Cast();
-				
-
-            if (Menu.Item("JcUseQ", true).GetValue<bool>() && Q.IsReady())
+            if (AIO_Menu.Champion.Jungleclear.UseQ && Q.IsReady())
                 Q.Cast(Mobs[0]);
         }
 
@@ -341,29 +267,23 @@ namespace ALL_In_One.champions
             if (Q.IsReady())
                 damage += Q.GetDamage(enemy) * 2;
 				
-            if (R.IsReady() && Menu.Item("CbUseR", true).GetValue<bool>())
-                damage += R.GetDamage(enemy);
-				
             if (W.IsReady())
                 damage += W.GetDamage(enemy);
+								
+			if(E.IsReady())
+                damage += (float)Player.GetAutoAttackDamage(enemy, true) * 2;
 				
+            if (R.IsReady() && AIO_Menu.Champion.Combo.UseR)
+                damage += R.GetDamage(enemy);
+
             if (Items.CanUseItem((int)ItemId.Tiamat_Melee_Only))
-		{
-		damage += (float)Player.GetItemDamage(enemy, Damage.DamageItems.Tiamat);
-		damage += (float)Player.GetAutoAttackDamage(enemy, true);
-		}
+				damage += (float)Player.GetItemDamage(enemy, Damage.DamageItems.Tiamat) + (float)Player.GetAutoAttackDamage(enemy, true);
 			
             if (Items.CanUseItem((int)ItemId.Ravenous_Hydra_Melee_Only))
-		{
-		damage += (float)Player.GetItemDamage(enemy, Damage.DamageItems.Hydra);
-		damage += (float)Player.GetAutoAttackDamage(enemy, true);
-		}
+				damage += (float)Player.GetItemDamage(enemy, Damage.DamageItems.Hydra) + (float)Player.GetAutoAttackDamage(enemy, true);
 
             if(!Player.IsWindingUp)
                 damage += (float)Player.GetAutoAttackDamage(enemy, true);
-
-	    if(E.IsReady())
-                damage += (float)Player.GetAutoAttackDamage(enemy, true) * 2;
 				
             return damage;
         }

c2b59b11e4ae058055f56de9a02bbb6b779b6395
rl144 <koreacnc3@gmail.com>
2015-05-11 11:52:12 +0900

    정상화

--- ALL In One/utility/SetOrb.cs
+++ ALL In One/utility/SetOrb.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using LeagueSharp;
 using LeagueSharp.Common;
 
@@ -9,23 +8,30 @@ namespace ALL_In_One.utility
 {
 	class SetOrb
 	{
-	        static Orbwalking.Orbwalker Orbwalker { get { return AIO_Menu.Orbwalker; } }
-	        static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
-	        internal static Menu Menu {get{return AIO_Menu.MainMenu_Manual.SubMenu("Champion").SubMenu("Orbwalker");}}
-	        internal static void Load()
-	        {
-			Menu.AddSubMenu(new Menu("SetCbMove", "SetCbMove"));
-			Menu.AddSubMenu(new Menu("SetHrMove", "SetHrMove"));
-			Menu.AddSubMenu(new Menu("SetCbAttack", "SetCbAttack"));
+		static Orbwalking.Orbwalker Orbwalker { get { return AIO_Menu.Orbwalker; } }
+		static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+		internal static Menu Menu {get{return AIO_Menu.MainMenu_Manual.SubMenu("Champion").SubMenu("Orbwalker");}}
+		internal static void Load()
+		{
+			Menu.AddSubMenu(new Menu("Set", "Set"));
+			Menu.SubMenu("Set").AddItem(new MenuItem("SetCbMove", "SetCbMove")).SetValue(true);
+			Menu.SubMenu("Set").AddItem(new MenuItem("SetHrMove", "SetHrMove")).SetValue(true);
+			Menu.SubMenu("Set").AddItem(new MenuItem("SetCbAttack", "SetCbAttack")).SetValue(true);
 			Game.OnUpdate += Game_OnUpdate;
-	        }
-			
+		}
+		
 		internal static void Game_OnUpdate(EventArgs args)
 		{
 			if (Player.IsDead)
 				return;
-			Orbwalker.SetMove(!(Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && SetCbMove) || !(Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed && SetHrMove));
-			Orbwalker.SetAttack(!(Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && SetCbAttack))
+			if((Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && !Menu.Item("SetCbMove").GetValue<bool>()) || (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed && !Menu.Item("SetHrMove").GetValue<bool>()))
+			Orbwalker.SetMovement(false);
+			else
+			Orbwalker.SetMovement(true);
+			if(Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Combo && !Menu.Item("SetCbAttack").GetValue<bool>())
+			Orbwalker.SetAttack(false);
+			else
+			Orbwalker.SetAttack(true);
 		}
 	}
 }

353af6b896f7ff178bc9cd1d3815239cebc66a59
rl144 <koreacnc3@gmail.com>
2015-05-17 00:12:59 +0900

    -_-

--- ALL In One/champions/Talon.cs
+++ ALL In One/champions/Talon.cs
@@ -24,7 +24,7 @@ namespace ALL_In_One.champions
             E = new Spell(SpellSlot.E, 700f, TargetSelector.DamageType.Physical);
             R = new Spell(SpellSlot.R, 600f, TargetSelector.DamageType.Physical) {Delay = 0.1f, Speed = 902f};
 
-            W.SetSkillshot(0.25f, 60f * (float)Math.PI / 180, 902f, false, SkillshotType.SkillshotCone);
+            W.SetSkillshot(0.25f, 60f * (float)Math.PI / 180, 1500f, false, SkillshotType.SkillshotCone);
             E.SetTargetted(0.25f, float.MaxValue);
             
             AIO_Menu.Champion.Combo.addUseQ();
@@ -148,7 +148,11 @@ namespace ALL_In_One.champions
                 return;
 
             if (AIO_Menu.Champion.Harass.UseW && W.IsReady())
-                W.CastOnBestTarget(0f, false, true);
+			{
+                var Wtarget = TargetSelector.GetTarget(W.Range, W.DamageType);
+				if(Wtarget != null)
+                W.ConeCast(Wtarget,50f);
+			}
         }
         
         static void Laneclear()
